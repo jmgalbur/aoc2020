40f4a926b6bcb9c79f5d0bcac0c376a65bc460d1
Jeremiah Murphy <jeremiah.g.murphy@gmail.com>
2020-12-07 13:25:05 -0500

    Add day 6 code

--- 06/aoc.js
+++ 06/aoc.js
@@ -15,24 +15,50 @@ const insideRange = (min,max)=>x=> min<=x && x <=max
 /* typical input flow */
 //	input = input.split("\n").map(Number).map(x=>Math.floor(x/3)-2).reduce((acc,next)=>acc+next)
 
+/* new after challenge */
+// join: Array.prototype.join("between")
+
 /////////
 
-/* DESCRIPTION HERE */
+/* People list their favorite letters of the alphabet. How many letters does each group like, all summed up? */
 
 function main() {
 	let output
 	let input = getIn()
+	let groups = input.split("\n\n")
+	groups = groups.map(x=>{
+		let oneline = x.replace(/\s+/g,"")
+		oneline = oneline.split("")
+		// the internet's telling me I should replace this with [...oneline], since split doesn't handle surrogate pairs, whereas spread operator does, since it uses String's iterator, which does
+		oneline.sort()
+		oneline = oneline.join("")
+		let yesQs = oneline.match(/(\w)\1*/g)
+		return yesQs.length
+	})
+	// yeah, this shouldn't go back into groups: more like groups_vote_num
 
-	output = NaN
+	output = groups.reduce(sumAll)
+	// FINALLY: some handiness, with a spritz of dandy
 	displayOut(1, output)
 }
 
-/* DESCRIPTION HERE */
+/* Silly billy, you that read wrong. How many letters does each group all like, all summed up? */
 
 function main2() {
 	let output
 	let input = getIn()
+	let groups = input.split("\n\n")
+	groups = groups.map(x=>{
+		let splits = x.split("\n")
+		let num = splits.length
+		let oneline = splits.join("")
+		oneline = oneline.split("")
+		oneline.sort()
+		oneline = oneline.join("")
+		let allYesQs = oneline.match(/(\w)\1*/g)
+		return allYesQs.filter(x=>x.length == num).length
+	})
 
-	output = NaN
+	output = groups.reduce(sumAll)
 	displayOut(2, output)
 }
