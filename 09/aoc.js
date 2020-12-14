375ff7a6d1709f2f9faa43f87d87ce0c8b01d281
Jeremiah Murphy <jeremiah.g.murphy@gmail.com>
2020-12-14 04:10:27 -0500

    09-scratch

--- 09/aoc.js
+++ 09/aoc.js
@@ -28,22 +28,59 @@ const insideRange = (min,max)=>x=> min<=x && x <=max
 
 /////////
 
-/* DESCRIPTION HERE */
+/* It's an older code, sir, but it checks out. Except for the glaring vulnerability that has its own CVE entry. What's the first number that's not a sum of two of many predecessors? */
 
 function main() {
 	let output
 	let input = getIn()
+	let numbers = input.split("\n").map(Number)
+	let preamble_length = 25
 
-	output = NaN
+	function isValidByPreamble(n, pre) {
+		return pre.some((x,i,arr)=>
+			[...arr.slice(0,i), ...arr.slice(i+1)].includes(n-x)
+			// don't double count x
+		)
+		// there is a/some number "x" in the preamble such that there's also an "n-x" in the preamble
+	}
+	// I tried to do everything all in one nested arrow function, but it became impossible to parse
+	// I def like having this separated out
+
+	output = numbers.slice(preamble_length).find((x,i)=>
+		!isValidByPreamble(x,numbers.slice(i,i+preamble_length))
+	)
+	// to be clear, the i=0 of .find() starts at the first number after the preamble (because we're iterating on the post-preamble-slice(), not on `numbers`)
+	// however this `i` also corresponds nicely to the position of the corresponding "preamble" of the current number `x` we're checking!
+
 	displayOut(1, output)
+	main_1_answer = output
 }
+
+let main_1_answer
+// so we only have to do that once, lol
 
 /* DESCRIPTION HERE */
 
 function main2() {
 	let output
 	let input = getIn()
+	let numbers = input.split("\n").map(Number)
 
-	output = NaN
+	let startI = 0
+	let endI = 2
+	// problem says at least 2 contiguous numbers. I had this at 1 and found the right answer, but that might not have worked for someone else's input. So fixed the bug
+	let sum = numbers.slice(startI,endI).reduce(sumAll)
+	// I realize this is expensive and wasteful for updating the sum, but it works well enough and intuitively conveys the meaning, more than `+= numbers[endI]` or whatever it would be
+
+	while(sum != main_1_answer) {
+		if (sum < main_1_answer) endI++
+		else endI = 2 + (++startI)
+		sum = numbers.slice(startI,endI).reduce(sumAll)
+		// always make sure to update what you're while'ing on!
+	}
+
+	output = numbers.slice(startI,endI).sort(numSort)
+	output = output[0]+output.slice(-1)[0]
+	// .pop() would also work for the second one, but it's also destructive, so nah
 	displayOut(2, output)
 }
